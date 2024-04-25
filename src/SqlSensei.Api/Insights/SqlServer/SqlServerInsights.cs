3f3b9c0ea6350f7c0b344694ed2877c90fcdfe3d
Marko Stojkov <mst@mca.dev>
2024-04-25 22:05:19 +0200

    .

--- src/SqlSensei.Api/Insights/SqlServer/SqlServerInsights.cs
+++ src/SqlSensei.Api/Insights/SqlServer/SqlServerInsights.cs
@@ -112,7 +112,7 @@ namespace SqlSensei.Api.Insights
 
             return Result.Ok(new InsightsResponse(
                 SqlServerServerCheckIssues.GetSqlServerChecks(serverIssues),
-                SqlServerServerPerformanceCheckIssues.GetSqlServerPerformanceFindings(queries, waits),
+                SqlServerServerPerformanceCheckIssues.GetSqlServerPerformanceFindings(queries, topWaitType),
                 SqlServerIndexIssues.GetSqlServerChecks(indexUsage, missingIndex)));
         }
 
